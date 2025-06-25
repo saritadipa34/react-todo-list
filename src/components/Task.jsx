3b1ae889b7a65402611c7f074dd9d065e2313116
saritadipa34 <saritadipa34@gmail.com>
2025-06-25 22:05:00 +0530

    Edit functionality modified

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,12 +1,12 @@
 import Button from "./Button";
 
-const Task=({todo,handleEdit})=>{
+const Task=({todo,handleEdit,handleDelete})=>{
     return(
         <div className="flex justify-between">
         <p className="h-12 w-3/4 bg-blue-400 flex px-2 items-center text-red-400 ">{todo}</p>
         <div className="flex gap-1">
 <Button text={"Edit"} onclick={handleEdit}/>
-<Button text={"Delete"}/>
+<Button text={"Delete"} onclick={handleDelete}/> 
         </div>
         </div>
     )
--- src/components/TodoBox.jsx
+++ src/components/TodoBox.jsx
@@ -6,16 +6,30 @@ const TodoBox=()=>{
     const [tasks,setTasks]=useState([]);
     const [inputValue,setInputValue]=useState("");
 
+const handleDelete=(index)=>{
+    const filtered=tasks.filter((item,i)=>{
+        return i != index;
+    });
+    setTasks(filtered);
+    console.log("delete");
+}
+
 const handleEdit=(index)=>{
 const editTask=tasks[index];
-const newTask = prompt("edit task",inputValue);
+const newTask = prompt("edit task",editTask);
+if(newTask !== null && newTask !== ""){
 tasks[index]=newTask;
 const updatedTask=[...tasks];
 setTasks(updatedTask,newTask);
+} else {
+    alert("Invalid input");
+}
 }
 
     const handleAdd=()=>{
+        if(inputValue ==="") return;
         setTasks([...tasks,inputValue]);
+        setInputValue("");
     }
 
     const handleInput=(e)=>{
@@ -27,14 +41,14 @@ setTasks(updatedTask,newTask);
         <div className="max-h-[520px] w-[600px] bg-yellow-400 py-5 px-10" >
             <h1 className="text-2xl font-bold text-center mb-5">Todo List</h1>
             <div className="flex">
-            <input type="text" className="w-3/4 h-12 bg-green-300 mb-5" maxLength={20} 
+            <input type="text" className="w-3/4 h-12 bg-green-300 mb-5" maxLength={20} value={inputValue} 
             onChange={handleInput}/>
             <Button text={"Add"} onclick={handleAdd}/>
             </div>
             <div className="max-h-[300px] bg-red-500 flex flex-col gap-3 overflow-y-scroll ">
             {
                 tasks.map((task,index)=>{
-                    return <Task inputValue={inputValue} todo={task} key={index} handleEdit={()=>{handleEdit(index)}}/>
+                    return <Task inputValue={inputValue} todo={task} key={index} handleEdit={()=>{handleEdit(index)}} handleDelete={()=>{handleDelete(index)}}/>
                 })
             }
         </div>
